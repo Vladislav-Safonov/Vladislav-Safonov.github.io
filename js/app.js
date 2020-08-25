80fe7af262d77362b9ec6818a309b5fbb24676aa
Vladislav-Safonov <wlad144779@gmail.com>
2020-08-25 23:47:18 +0300

    v3

--- js/app.js
+++ js/app.js
@@ -1,20 +1,52 @@
-$(document).ready(function() {
-
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "../php/mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
+$('.block-button').click(function() {
+	event.preventDefault();
+	alert("К сожалению github (хостинг на котором находится мой сайт) не поддерживает метод передачи данных POST, поэтому сделать на нём обратную связь не составляет возможным.");
 });
+
+//Hide menu
+var headerMenu = $('.header-menu');
+
+scrollPrev = 0;
+
+$(window).scroll(function() {
+	var scrolled = $(window).scrollTop();
+
+		if(scrolled > 100 && scrolled > scrollPrev) {
+			$(headerMenu).addClass('hide-menu');
+		} else {
+			$(headerMenu).removeClass('hide-menu');
+		}
+
+	scrollPrev = scrolled;
+})
+
+//--------------------------
+
+/*scroll to section*/
+$(document).ready(function(){
+	$('#nav').on("click","a", function(event){
+		event.preventDefault();
+		var id = $(this).attr('href'),
+		 	top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top-100}, 1300);
+	});
+});
+/*------------------*/
+
+$(document).ready(function(){
+	$('.vshape-link').click(function(event){
+		event.preventDefault();
+		var id = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body, html').animate({scrollTop: top-100}, 1300);
+	});
+});
+
+
+$(window).scroll(function(){
+	var scrolled = $(this).scrollTop();
+
+	$('.header-text_block').css({
+		"transform" : "translate(0%, "+ scrolled/33 +"%"
+	});
+})
